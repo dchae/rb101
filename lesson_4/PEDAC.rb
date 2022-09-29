ec895218e832fbe37a6a4b7697f27e2bb811ce02
Daniel Chae <daniel.j.chae@gmail.com>
2022-09-29 01:30:09 -0700

    Lesson 4 - PEDAC

---
+++ lesson_4/PEDAC.rb
@@ -0,0 +1,15 @@
+# Understand the problem
+# Questions:
+#   - Only string inputs? i.e. do we need to sanitise inputs
+#   - Do spaces count?
+
+# Input: string
+# Output: array of substrings
+# Rules:
+#   Explicit requirements:
+#   - every palindrome in the string must be returned
+#   - palindromes are case sensitive
+#   Implicit requirements:
+#   - return in an array
+#   - no palindromes in string returns empty array
+#   - palindromes are at least two chars long
